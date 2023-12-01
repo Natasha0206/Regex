272c13ecd02968004b5cb02b62bda73c56d7e192
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-12-01 11:02:11 +0530

    Add files via upload

---
+++ Matching_whitespace.cpp
@@ -0,0 +1,32 @@
+/**\s matches any whitespace character [ \r\n\t\f ].
+
+ach05_01.png
+
+
+\S matches any non-white space character.
+
+ach05_02.png
+
+Task
+
+You have a test string . Your task is to match the pattern 
+Here,  denotes whitespace characters, and  denotes non-white space characters.
+
+Note
+
+This is a regex only challenge. You are not required to write code.
+You have to fill the regex pattern in the blank (_________)**/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    Regex_Pattern = r"\S\S\s\S\S\s\S\S"  
+    return 0;
+}
