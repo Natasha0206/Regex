4e2d10bcdcc037d4f4a4f25c7ebc937b5ddcdc97
Natasha Kumari <140797294+Nxtxshx@users.noreply.github.com>
2023-12-01 11:03:44 +0530

    Add files via upload

---
+++ Matching_Digits.cpp
@@ -0,0 +1,36 @@
+/**The expression \d matches any digit [ - ].
+
+ach04_01.png
+
+
+
+The expression \D matches any character that is not a digit.
+
+ach04_02.png
+
+
+Task
+
+You have a test string . Your task is to match the pattern 
+Here  denotes a digit character, and  denotes a non-digit character.
+
+Note
+
+This is a regex only challenge. You are not required to write any code.
+You only have to fill the regex pattern in the blank (_________).**/
+
+
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    Regex_Pattern = r"\d\d(.)\d\d(.)\d\d\d\d"
+    return 0;
+}
